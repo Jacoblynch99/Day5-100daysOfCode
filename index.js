266073b739c6f96d87956778dfd7ec5745914985
Jacoblynch99 <jacoblynch99@gmail.com>
2020-09-15 08:07:26 -0500

    done

---
+++ index.js
@@ -0,0 +1,18 @@
+const _ = require("underscore")
+
+const numArr = [53, 67, 97, 100, 81, 2, 25, 3]
+
+const reducedArr = (a, b) => {
+  return `${a} ${b}`
+}
+
+const findCondition = (num) => {
+  return num % 3 === 0
+}
+
+const foundNum = _.find(numArr, findCondition) // this goes through aa array until the condition, which set by a function, is met, and only returns the first one that passes
+
+const reversedStr = _.reduceRight(numArr, reducedArr) // like reduce except it starts from the right and goes left
+
+console.log(reversedStr)
+console.log(foundNum)
